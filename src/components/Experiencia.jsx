562bf4e3272a0f69c0b95210090e3c71e984661b
UtopicRay <ray2021@gmail.com>
2024-06-08 20:33:20 -0400

    'Experiencia 100%'

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -9,20 +9,20 @@ import {
 import NetFlix from "../assets/img/Netflix-Clon.webp";
 export function Habilidades() {
   return (
-    <div className="max-w-[1000px] grid grid-cols-2 mx-auto text-left gap-8 place-item-center">
+    <div className="max-w-[1000px] grid md:grid-cols-2 grid-cols-1 mx-auto text-left gap-8 place-item-center">
       <div className='p-6'>
         <h2 className="my-2 font-bold">Experiencia</h2>
-        <p className='mb-4'>
+        <p className='my-4'>
           Durante la universidad, participé en un equipo de desarrollo de
           software. Durante este período, las actividades que me fueron
           asignadas incluyeron la implementación de funcionalidades como la
-          creación y validación de formularios, el manejo de información de
+          creación de API Rest, validación de formularios, el manejo de información de
           bases de datos y la implementación de un sistema de recomendación
           utilizando PHP como lenguaje de programación. Sin embargo, no he
           tenido la oportunidad de trabajar fuera del entorno escolar, por lo
-          que espero aprender y mejorar mis habilidades como programador.
+          que espero seguir aprendiendo y mejorando mis habilidades como programador.
         </p>
-        <div className="md:flex flex-warp justify-center gap-4 text-6xl mb-8">
+        <div className="flex flex-warp justify-center gap-4 text-6xl mb-8">
           <DiHtml5 className="text-orange-500 hover:scale-110 transition-all  duration-300"></DiHtml5>
           <DiCss3 className="text-blue-500 hover:scale-110 transition-all duration-300"></DiCss3>
           <DiJavascript1 className="text-yellow-500 hover:scale-110 transition-all duration-300"></DiJavascript1>
@@ -31,25 +31,11 @@ export function Habilidades() {
           <DiGit className="text-red-600 hover:scale-110 transition-all duration-300"></DiGit>
         </div>
       </div>
-      <div className="relative group max-w-[600px]">
-        <div class="w-full h-full absolute -inset-1 bg-gradient-to-r from-blue-500 to-[#FFDA78] blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
-        <div className="w-full p-4 bg-white relative bg-opacity-10 backdrop-blur-lg rounded-lg">
-          <img
-            src={NetFlix}
-            alt="foto del Clon de Netflix"
-            className="rounded-lg md:max-w-[360px]"
-          ></img>
-        </div>
-      </div>
-
       <div className="relative group">
-        <div class="w-full h-full absolute -inset-1 bg-gradient-to-r from-blue-500 to-[#FFDA78] blur opacity-25 group-hover:opacity-100 transition duration-300"></div>
-        <div className="w-full p-4 bg-white relative bg-opacity-10 backdrop-blur-lg rounded-lg ">
-          <img
-            src={NetFlix}
-            alt="foto del Clon de Netflix"
-            className="rounded-lg md:max-w-[360px]"
-          ></img>
+      <div className="absolute -inset-1 place-items-center  w-full bg-gradient-to-r from-purple-100 to-orange-500 rounded-lg backdrop-blur-lg blur opacity-25 group-hover:opacity-100 transition-all duration-300">
+      </div>
+        <div className="relative p-4 h-full bg-white bg-opacity-25 backdrop-blur-lg rounded-lg">
+        <img className="h-full rounded-lg" src={NetFlix}></img>
         </div>
       </div>
     </div>
