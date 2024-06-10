98167666d897661011e6ab5edd5aac5105cb78e8
UtopicRay <ray2021@gmail.com>
2024-06-10 14:05:23 -0400

    'Hero 90%'

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,57 +8,77 @@ import {
   DiGit,
   DiBootstrap,
   DiJavascript1,
+  DiPhp,
 } from "react-icons/di";
 import { MdEmail } from "react-icons/md";
+import { FaVuejs } from "react-icons/fa";
+import { SiAstro, SiPostgresql, SiTailwindcss } from "react-icons/si";
+import { RiFirebaseFill } from "react-icons/ri";
 export function Hero() {
   return (
     <section
       className="max-w-[1200px] mx-auto grid md:grid-cols-8 gap-6 p-10 md:p-0 mb-16 mt-36"
       id="home"
     >
-      <div className="grid grid-cols-2 col-span-5 p-8 glass">
+      <div className="grid md:grid-cols-2 grid-cols-1 md:col-span-5 col-span-8 p-8 glass">
         <div className="place-items-center">
           <img className="h-auto aspect-auto" src={Avatar}></img>
         </div>
         <div className="mx-2 flex justify-center items-start flex-col">
-          <p className="text-2xl md:text-3xl font-bold text-left">
+          <p className="text-xl md:text-3xl font-bold text-left">
             Hola, soy Ray Rendón<br></br>
-            <TypeAnimation
-              sequence={[
-                "Desarrollador Frontend",
-                1000,
-                "Ing. Ciencias Informáticas",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-            ></TypeAnimation>
+            <span className="text-2xl font-bold text-left">
+              <TypeAnimation
+                sequence={[
+                  "Desarrollador FullStack Jr.",
+                  1000,
+                  "Ing. Ciencias Informáticas",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              ></TypeAnimation>
+            </span>
           </p>
-          <button className="bg-[#2A629A] px-4 py-2 rounded-lg text-gray-200 hover:bg-slate-700 duration-300 mt-16">
-            Descargar CV
-          </button>
+          <a href="/document/CV.pdf" download="Ray Rendón Mesa Currículum.pdf">
+            <button className="bg-[#2A629A] px-4 py-2 rounded-lg text-gray-200 hover:bg-slate-700 duration-300 mt-16">
+              Descargar CV
+            </button>
+          </a>
+          <div className="flex md:justify-center justify-start items-center mt-4">
+            <a
+              className="text-white  hover:scale-110 transition-all duration-200 underline"
+              href="mailto:rayrm2021@gmail.com?subject=Interesado%20en%20sus%20servicios"
+            >
+              rayrm2021@gmail.com
+            </a>
+            <MdEmail className="text-gray-500 text-2xl mx-2 hover:scale-125 transition-all duration-300"></MdEmail>
+          </div>
         </div>
       </div>
 
-      <div className="grid md:col-span-3  gap-6">
-        <div className="text-5xl md:p-12 p-6 glass w-full">
-          <h2 className="font-bold text-center mb-4">
-            Habilidades
-          </h2>
-          <div className="grid grid-cols-4 gap-y-4 gap-x-5">
+      <div className="grid md:col-span-3 col-span-8  gap-6">
+        <div className="text-5xl md:p-12 p-6 glass w-full ">
+          <h2 className="font-bold text-center mb-4">Habilidades</h2>
+          <div className="grid grid-cols-4 gap-y-4 gap-x-5 ">
             <DiHtml5 className="text-orange-500 hover:scale-110 transition-all duration-200"></DiHtml5>
             <DiCss3 className="text-blue-500 hover:scale-110 transition-all duration-200"></DiCss3>
             <DiJavascript1 className="text-yellow-500 hover:scale-110 transition-all duration-200"></DiJavascript1>
             <DiReact className="text-blue-600 hover:scale-110 transition-all duration-200"></DiReact>
+            <FaVuejs className="text-green-500 hover:scale-110 transition-all duration-200"></FaVuejs>
+            <SiAstro className="text-orange-500 text-4xl hover:scale-110 transition-all duration-200" />
+            <SiTailwindcss className="text-blue-300 hover:scale-110 transition-all duration-200"></SiTailwindcss>
             <DiBootstrap className="text-purple-700 hover:scale-110 transition-all duration-200"></DiBootstrap>
             <DiGit className="text-red-600 hover:scale-110 transition-all duration-200"></DiGit>
+            <DiPhp className="text-gray-300 hover:scale-110 transition-all duration-200"></DiPhp>
+            <SiPostgresql className="text-blue-700 hover:scale-110 transition-all duration-200"></SiPostgresql>
+            <RiFirebaseFill className="text-yellow-300 hover:scale-110 transition-all duration-200"></RiFirebaseFill>
           </div>
         </div>
         <div className="text-5xl gap-4 flex justify-center items-center glass text-white p-2 ">
           <AiFillGithub className="hover:scale-110 transition-all duration-200"></AiFillGithub>
           <AiFillLinkedin className="hover:scale-110 transition-all duration-200"></AiFillLinkedin>
-          <MdEmail className="hover:scale-110 transition-all duration-200"></MdEmail>
         </div>
       </div>
     </section>
